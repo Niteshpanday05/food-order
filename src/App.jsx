bbe58523c9bca40b53f82ac7f59ca9b2c8566f8a
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-28 21:27:04 +0545

    some changes in ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Test from "./components/test/Test";
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path = '/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path ='/placeorder' element={<PlaceOrder/>} />
-      </Routes>
-    
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer/>
+    </>
   );
 };
 
