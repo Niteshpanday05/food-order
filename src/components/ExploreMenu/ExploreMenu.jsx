5c6a3327debc24500dc5e8626a919af34b3572ed
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-28 12:24:22 +0545

    problem in active link

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import {menu_list} from '../../assets/assets';
 import './ExploreMenu.css'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
+
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore Menu</h1>
       
       <p className='explore-menu-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore facere enim quia optio necessitatibus..</p>
       <div className='explore-menu-list'>
-        {menu_list.map((item, index)=>{
+        {menu_list.map((item,index)=>{
             return (
-                <div className="explore-menu-list-item">
-                    <img src={item.menu_image} alt="" />
+                <div onClick={()=>setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)}  key={index} className="explore-menu-list-item">
+                    <img className={category === item.menu_name ? "active":""} src={item.menu_image} alt={item.menu_name} />
                     <p>{item.menu_name}</p>
                 </div>
             )
