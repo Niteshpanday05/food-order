4a1cef24d340a190147effb76dff4d3fe50cdddf
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-30 15:45:40 +0545

    carousal

---
+++ src/components/Carousal/Carousal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carousal = () => {
+  return (
+    <div>
+      this is carousal <section></section>
+    </div>
+  )
+}
+
+export default Carousal
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
+
 const Navbar = () => {
   const menus = ["Home","Menu","Services", "About", "Contact"];
   const [menu, setMenu] = useState("Home");
@@ -10,13 +11,13 @@ const Navbar = () => {
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
         {menus.map((item) => (
-          <Link
+          <a
             key={item}
             className={menu === item ? "active" : ""}
             onClick={() => setMenu(item)}
           >
              {item.charAt(0).toUpperCase() + item.slice(1)}
-          </Link>
+          </a>
         ))}
       </ul>
       <div className="navbar-right">
