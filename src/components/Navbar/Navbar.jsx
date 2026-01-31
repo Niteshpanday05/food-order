f4d8fca80c01619437ab5a08b21a2a688a591be8
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-31 23:56:47 +0545

    navbar navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
-
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-  const menus = ["Home","Menu","Services", "About", "Contact"];
+  const menus = [
+    { name: "home", path: "/" },
+    { name: "about", path: "/about" },
+    { name: "contact", path: "/contact" },
+    { name: "menu", path: "/menu" },
+    { name: "services", path: "/services" },
+  ];
   const [menu, setMenu] = useState("Home");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
+      import {NavLink} from "react-router-dom";
       <ul className="navbar-menu">
         {menus.map((item) => (
-          <a
-            key={item}
-            className={menu === item ? "active" : ""}
-            onClick={() => setMenu(item)}
+          <NavLink
+            key={item.name}
+            to={item.path}
+            className={({ isActive }) => (isActive ? "active" : "")}
           >
-             {item.charAt(0).toUpperCase() + item.slice(1)}
-          </a>
+            {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
+          </NavLink>
         ))}
       </ul>
       <div className="navbar-right">
