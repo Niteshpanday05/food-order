6c847497bf246d8c4ce3eeeb10e6b0bb66179a82
Niteshpanday05 <eagleseye.np@gmail.com>
2026-02-10 16:17:53 +0545

    making reusable componnet

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,45 +1,59 @@
-import React from 'react'
-import './Footer.css'
-import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='footer' id="footer">
-        <div className="footer-content">
-            <div className="footer-content-left">
-                <Link to='/' className="logo"> Food <span>Express</span></Link> 
-                <p>Food Express brings delicious meals straight to your doorstep, fast and fresh. We partner with trusted local restaurants to ensure every bite is tasty and satisfying.</p>
-                <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt="" />
-                    <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
-                </div>
-            </div>
-            <div className="footer-content-center">
-                <h2>COMPANY</h2>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    
-                    <li>Delivery</li>
-                    <li>Privacy policy</li>
-                </ul>
-
-            </div>
-            <div className="footer-content-right">
-                <h2>GET IN TOUCH</h2>
-                <ul>
-                    <li>+977-9702003949</li>
-                    <li>niteshpanday55555@gmail.com</li>
-                </ul>
-
-            </div>
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <Link to="/" className="logo">
+            {" "}
+            Food <span>Express</span>
+          </Link>
+          <p>
+            Food Express brings delicious meals straight to your doorstep, fast
+            and fresh. We partner with trusted local restaurants to ensure every
+            bite is tasty and satisfying.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/delivery">Delivery</Link>
+            </li>
+            <li>
+              <Link to="/privacy-policy">Privacy Policy</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+977-9702003949</li>
+            <li>niteshpanday55555@gmail.com</li>
+          </ul>
+        </div>
+      </div>
       <hr />
-      <p className='footer-copyright'>Copyright 2024 @ Tomato.com- All Right Reserved</p>
+      <p className="footer-copyright">
+        Copyright 2024 @ Tomato.com- All Right Reserved
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
